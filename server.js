43a125d42a7f87bbbec683db72c85c0786fa27db
Lemniscatus <78922113+lemniiscatus@users.noreply.github.com>
2021-03-30 13:02:13 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+socket.on("connect", function() {
+   console.log("Connected to Socket I/O Server!");
+   console.log(name + " wants to join  " + room);
+   // to join a specific room
+   socket.emit('joinRoom', {
+     name: name,
+     room: room
+   });
+ });
